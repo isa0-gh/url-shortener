9d7e28ae0029a8974c7d364a594ec1a096a6a29d
isa0 <0isa@proton.me>
2025-11-01 15:00:59 +0300

    Added default port

--- main.go
+++ main.go
@@ -14,7 +14,6 @@ func main() {
 	var port string = os.Getenv("PORT")
 	var bindAddress string
 	if port == "" {
-	
 		fmt.Println("Default PORT(8080) will be used. To change port: PORT=500 ./urlshorter")
 		bindAddress = fmt.Sprintf(":%s", "8080")
 	} else {
